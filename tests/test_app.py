c59751031eee4aaa806c222bc9fc4283aec28684
Gabriel Lima <ga97lima@gmail.com>
2025-06-17 18:39:27 -0300

    fazendo um CRUD basico de cadastro de usuario

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,62 @@
-from fastapi.testclient import TestClient
 from http import HTTPStatus
-from fastapi_new.app import app
-
-client = TestClient(app)
 
 
-def test_read_root():
-    response = client.get('/')
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testuser',
+            'email': 'gabruie@gmail.com',
+            'password': 'testpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'testuser',
+        'email': 'gabruie@gmail.com',
+        'id': 1,
+    }
+
+
+def test_read_user(client):
+    response = client.get('/users/')
+
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'Message': 'Hello World'}
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'testuser',
+                'email': 'gabruie@gmail.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'updateduser',
+            'email': 'update@test.com',
+            'password': 'newpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'updateduser',
+        'email': 'update@test.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'updateduser',
+        'email': 'update@test.com',
+        'id': 1,
+    }
