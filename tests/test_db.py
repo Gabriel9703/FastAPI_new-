f1d5fede06ff7c43380500e4e8714ea9e4e0ad4d
Gabriel Lima <ga97lima@gmail.com>
2025-06-24 13:24:17 -0300

    criação de teste para confirmar o cadastro do user no db

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,10 +1,24 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
 from fastapi_new.models import User
 
 
-def test_create_user():
-    user = User(
-        username='testuser', email='gaheb@gm.com', password='testpassword'
-    )
-    assert user.username == 'testuser'
-    assert user.email == 'gaheb@gm.com'
-    assert user.password == 'testpassword'
+def test_create_user(session, mook_db_time):
+    with mook_db_time(model=User) as time:
+        new_user = User(
+            username='alice', password='secret', email='teste@test'
+        )
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'alice'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'teste@test',
+        'password': 'secret',
+        'created_at': time,
+    }
